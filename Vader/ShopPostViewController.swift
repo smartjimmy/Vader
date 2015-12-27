d75ace53b447fcca414430e82cb641047b4946f6
James Kang <jimmyjameskang@gmail.com>
2015-12-27 15:29:03 -0800

    Second commit added shop post VC

---
+++ Vader/ShopPostViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ShopPostViewController.swift
+//  Vader
+//
+//  Created by James Kang on 12/27/15.
+//  Copyright © 2015 James Kang. All rights reserved.
+//
+
+import UIKit
+
+class ShopPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
