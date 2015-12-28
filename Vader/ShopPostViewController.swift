80074e3807f5902b57099c016036d4d50a97aed9
James Kang <jimmyjameskang@gmail.com>
2015-12-27 20:12:57 -0800

    Added functionality for the shopPostVC does not push to parse yet but alot of the logic is down

--- Vader/ShopPostViewController.swift
+++ Vader/ShopPostViewController.swift
@@ -8,28 +8,94 @@
 
 import UIKit
 
-class ShopPostViewController: UIViewController {
-
+class ShopPostViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var itemNameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Text Fields logic and jumps
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == itemNameTextField {
+            descriptionTextField.becomeFirstResponder()
+        } else if textField == descriptionTextField {
+            priceTextField.becomeFirstResponder()
+            
+        }
+        else {
+            priceTextField.resignFirstResponder()
+        }
+        return true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //Cancel Button tapped
+    @IBAction func cancelTapped(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
+    
+    //Post Button tapped
+    @IBAction func postTapped(sender: AnyObject) {
+        
+        if self.itemNameTextField.text!.isEmpty {
+            noItemAlert()
+        } else {
+            if self.descriptionTextField.text!.isEmpty {
+                noDescriptionAlert()
+            } else {
+                if self.priceTextField.text!.isEmpty {
+                    noPriceAlert()
+                }
+            }
+        }
+    }
+    
+    //alert for empty item name text field
+    func noItemAlert() {
+        let alert = UIAlertController(title: "Item Name", message: "Please include the name of your item", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel) {
+            UIAlertAction in
+            alert.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        alert.addAction(cancelAction)
+        
+       self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    //alert for empty description text field
+    func noDescriptionAlert() {
+        let alert = UIAlertController(title: "Description", message: "Please include a description of your item", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel) {
+            UIAlertAction in
+            alert.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        alert.addAction(cancelAction)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    //alert for empty price text field
+    func noPriceAlert() {
+        let alert = UIAlertController(title: "Price", message: "Please include the price of your item", preferredStyle: .Alert)
+        
+        let cancelAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel) {
+            UIAlertAction in
+            alert.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        alert.addAction(cancelAction)
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
